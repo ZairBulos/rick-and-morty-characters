0af1809ee5ef3c3e505108c88462f8dfb8002337
ZairBulos <buloszair@gmail.com>
2024-04-23 22:05:43 -0300

    feat: add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header class="p-8 text-center bg-white">
+    <h1 class="text-4xl font-extrabold text-[#303032]">
+      Rick and Morty Characters
+    </h1>
+  </header>
+</template>
+
+<style scoped></style>
